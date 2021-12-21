f3c33065126e0f73e2d9946f906455b95c70bf64
Douglas Braga <30268535+dobraga@users.noreply.github.com>
2021-12-21 05:00:02 -0300

    install packages

--- install_packages.sh
+++ install_packages.sh
@@ -1,4 +1,7 @@
-sudo apt install -y zsh locales-all
+wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+
+sudo apt install -y zsh locales-all r-base libcurl4-openssl-dev libsodium-dev
 
 sh -c "$(curl -fsSL https://pyenv.run)"
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
