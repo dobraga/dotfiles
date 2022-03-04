ac5b42080119629a88aef66258c4ea0b02697d06
Douglas Braga <30268535+dobraga@users.noreply.github.com>
2022-03-04 20:23:24 -0300

    melhorias gerais

--- config.sh
+++ config.sh
@@ -1,4 +1,13 @@
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH/plugins/zsh-autosuggestions
+
 rm -f ~/.zshrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
+
 rm -f ~/.gitconfig
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+
+rm -f ~/.config/lvim/config.lua
+ln -s ~/dotfiles/config.lua ~/.config/lvim/config.lua
+
+poetry config virtualenvs.in-project true
+
--- install_packages.sh
+++ install_packages.sh
@@ -1,10 +1,25 @@
+# Adiciona repository do R e neovim
 wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
 sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+sudo add-apt-repository ppa:neovim-ppa/unstable
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
 
-sudo apt install -y zsh locales-all r-base libcurl4-openssl-dev libsodium-dev
+sudo apt-get update
 
+sudo apt install -y zsh locales-all r-base libcurl4-openssl-dev libsodium-dev cargo ripgrep neovim fzf nodejs
+
+# Instala fontes
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/DroidSansMono.zip
+unzip DroidSansMono.zip -d ~/.fonts
+fc-cache -fv
+
+# Instala libs em rust
+cargo install bat exa ytop
+
+# Instala pyenv, poetry, starship
 sh -c "$(curl -fsSL https://pyenv.run)"
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
-git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH/plugins/zsh-autosuggestions"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
