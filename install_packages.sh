405a3ae1f6d5e27b8c32e3a4a581ed9f8bbbcedf
Douglas Braga <30268535+dobraga@users.noreply.github.com>
2022-03-08 23:06:11 -0300

    add chrome install

--- install_packages.sh
+++ install_packages.sh
@@ -6,9 +6,13 @@ sudo add-apt-repository ppa:gnome-terminator
 curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
 curl -fsSL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 
+# Adiciona repository do chrome
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+
 sudo apt-get update
 
-sudo apt install -y zsh locales-all r-base libcurl4-openssl-dev libsodium-dev cargo ripgrep neovim fzf nodejs terminator yarn
+sudo apt install -y zsh locales-all r-base libcurl4-openssl-dev libsodium-dev cargo ripgrep neovim fzf nodejs terminator yarn google-chrome-stable
 
 # Instala fontes
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/DroidSansMono.zip
