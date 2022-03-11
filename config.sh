408091dc5b3065af85fdee3cc5a5fdb42a228545
Douglas Braga <30268535+dobraga@users.noreply.github.com>
2022-03-11 19:35:36 -0300

    configs to folder

--- config.sh
+++ config.sh
@@ -1,23 +1,23 @@
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH/plugins/zsh-autosuggestions
 
 rm -f ~/.zshrc
-ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/configs/.zshrc ~/.zshrc
 
 rm -f ~/.gitconfig
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/configs/.gitconfig ~/.gitconfig
 
 rm -f ~/.config/lvim/config.lua
-ln -s ~/dotfiles/config.lua ~/.config/lvim/config.lua
+ln -s ~/dotfiles/configs/config.lua ~/.config/lvim/config.lua
 
 rm -rf ~/.config/terminator
 mkdir ~/.config/terminator
-ln -s ~/dotfiles/config.terminator ~/.config/terminator/config
+ln -s ~/dotfiles/configs/config.terminator ~/.config/terminator/config
 
 rm -rf ~/.config/lvim/lsp-settings
 mkdir ~/.config/lvim/lsp-settings
 
 echo "
-Para configuração do linter do python, execute:
+Para configuração do linter do python no lunarvim, execute:
 
 :NlspConfig pyright
 
