41a78e6f833dc8577ed9669b7a1da4cdde0badd2
Douglas Braga <30268535+dobraga@users.noreply.github.com>
2026-02-20 11:46:49 -0300

    install nvidia toolkit

--- install_nvdia.sh
+++ install_nvdia.sh
@@ -10,8 +10,13 @@ echo "Installing NVIDIA drivers..."
 sudo apt install -y ubuntu-drivers-common
 sudo ubuntu-drivers autoinstall
 
-# 3. Install CUDA Toolkit
-echo "Installing CUDA toolkit..."
-sudo apt install -y nvidia-cuda-toolkit
+# 3. Install container Toolkit
+echo "Installing container toolkit..."
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+sudo apt-get update
+sudo apt-get install -y nvidia-container-toolkit
 
 echo "Installation complete. PLEASE REBOOT YOUR SYSTEM."
